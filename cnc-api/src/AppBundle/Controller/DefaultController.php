b5ca6bd5bc6bc6a8b15708980fceb653d6f36ba9
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-10 15:29:01 +0100

    Added version route.

--- cnc-api/src/AppBundle/Controller/DefaultController.php
+++ cnc-api/src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,26 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/", defaults={"_format"="json"})
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/ehlo", name="ehlo")
+     * @Method({"GET"})
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'content' => array(
+                "version" => $this->container->getParameter('version')
+            )
         ));
     }
 }
