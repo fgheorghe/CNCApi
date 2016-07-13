1e325df0bb7e65eb3de382df57f3d687fbbce478
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-13 22:34:51 +0100

    Added back ehlo controller. Added phpunit archive.

--- cnc-api/src/AppBundle/Controller/DefaultController.php
+++ cnc-api/src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,41 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/", defaults={"_format"="json"})
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * Version endpoint.
+     *
+     * Returns a JSON object with the following format:
+     *
+     * {"version":
+     *  {"major":0,
+     *  "minor":0,
+     *  "revision":0}
+     * }
+     *
+     * @Route("/ehlo", name="ehlo")
+     * @Method({"GET"})
+     *
+     * @ApiDoc(
+     *  resource=true
+     * )
      */
-    public function indexAction(Request $request)
+    public function ehloAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        // Return version object.
+        return $this->render('default/index.html.twig', array(
+            'content' => array(
+                "version" => $this->container->getParameter('version')
+            )
+        ));
     }
 }
