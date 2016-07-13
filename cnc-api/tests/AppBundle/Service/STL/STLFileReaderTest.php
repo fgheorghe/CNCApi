f45c7ca943aab03613f4955c2329816cb2274c88
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-13 23:09:14 +0100

    Test file stub.

---
+++ cnc-api/tests/AppBundle/Service/STL/STLFileReaderTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Tests\Service\STL;
+
+use Symfony\Component\DependencyInjection\Container;
+use AppBundle\Service\STL\STLFileReader;
+
+class STLFileReaderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
