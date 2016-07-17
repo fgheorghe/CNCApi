e603a63c15785c38357cd63d8638f935d5db0fcf
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-17 13:09:39 +0100

    STL File conversion to array test.

--- cnc-api/tests/AppBundle/Service/STL/STLFileReaderTest.php
+++ cnc-api/tests/AppBundle/Service/STL/STLFileReaderTest.php
@@ -131,4 +131,55 @@ EOT;
             $stlFileReader->getFacetVertexCoordinates($this->facetNormal1)
         );
     }
+
+    function testConvertToArray() {
+        $stlFileReader = new STLFileReader(
+            $this->stlFileString
+        );
+
+        $this->assertEquals(
+            array(
+                "name" => "TEST",
+                "facet-normals" => array(
+                    array(
+                        "coordinates" => array(
+                            0,
+                            1,
+                            2
+                        ),
+                        "vertices" => array(
+                            array(
+                                3, 4, 5
+                            ),
+                            array(
+                                5, 4, 6
+                            ),
+                            array(
+                                7, 8, 9
+                            )
+                        )
+                    ),
+                    array(
+                        "coordinates" => array(
+                            10,
+                            11,
+                            12
+                        ),
+                        "vertices" => array(
+                            array(
+                                12, 14, 15
+                            ),
+                            array(
+                                16, 17, 18
+                            ),
+                            array(
+                                19, 20, 21
+                            )
+                        )
+                    )
+                )
+            ),
+            (array) $stlFileReader
+        );
+    }
 }
