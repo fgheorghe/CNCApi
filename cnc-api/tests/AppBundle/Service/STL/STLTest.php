fb0de65c5381f4146e84b51a1e87b18eadad1ce0
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-17 12:58:45 +0100

    Test for injecting editor dependency into STL library.

--- cnc-api/tests/AppBundle/Service/STL/STLTest.php
+++ cnc-api/tests/AppBundle/Service/STL/STLTest.php
@@ -5,6 +5,7 @@ namespace AppBundle\Tests\Service\STL;
 use Symfony\Component\DependencyInjection\Container;
 use AppBundle\Service\STL\STL;
 use AppBundle\Service\STL\STLFileReader;
+use AppBundle\Service\STL\STLEdit;
 
 class STLTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,10 +14,11 @@ class STLTest extends \PHPUnit_Framework_TestCase
         \Mockery::close();
     }
 
-    public function testContainerAndStlFileReaderServicesAreSet() {
+    public function testContainerStlEditAndStlFileReaderServicesAreSet() {
         // Service and stl file reader container mocks.
         $containerMock = \Mockery::mock(Container::class);
         $stlFileReaderMock = \Mockery::mock(STLFileReader::class);
+        $stlEditorMock = \Mockery::mock(STLEdit::class);
 
         // Prepare a mock of the setContainer method.
         $stlLibraryMock = \Mockery::mock(STL::class)
@@ -30,6 +32,10 @@ class STLTest extends \PHPUnit_Framework_TestCase
             ->with($stlFileReaderMock)
             ->once();
 
+        $stlLibraryMock->shouldReceive('setStlEditor')
+            ->with($stlEditorMock)
+            ->once();
+
         // Now get the original constructor.
         $reflectedStlLibraryClass = new \ReflectionClass(STL::class);
         $reflectedStlLibraryClassConstructor = $reflectedStlLibraryClass->getConstructor();
@@ -38,7 +44,8 @@ class STLTest extends \PHPUnit_Framework_TestCase
         $reflectedStlLibraryClassConstructor->invoke(
             $stlLibraryMock,
             $containerMock,
-            $stlFileReaderMock
+            $stlFileReaderMock,
+            $stlEditorMock
         );
     }
 }
