fa887647a87838ef17b3d0a48c3ead6fb2266cfe
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-17 12:57:09 +0100

    Added code for stl editing.

---
+++ cnc-api/src/AppBundle/Service/STL/STLEdit.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fgheorghe
+ * Date: 17/07/16
+ * Time: 12:55
+ */
+
+namespace AppBundle\Service\STL;
+
+/**
+ * Class STLEdit. Used for 'chopping' off parts of an STL file.
+ *
+ * NOTE: Only to be used for milling!
+ *
+ * @package AppBundle\Service\STL
+ */
+class STLEdit
+{
+    // TODO: Implement.
+}
