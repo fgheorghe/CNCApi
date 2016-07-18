d6393249bf1c0a4c1118fcc3df344c7d83f90af3
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-18 22:12:08 +0100

    Added logic for extracting the actual milling content.

--- cnc-api/src/AppBundle/Service/STL/STLMillingEdit.php
+++ cnc-api/src/AppBundle/Service/STL/STLMillingEdit.php
@@ -40,4 +40,114 @@ class STLMillingEdit
     public function __construct(array $stlFileContentArray) {
         $this->setStlFileContentArray($stlFileContentArray);
     }
+
+    /**
+     * Lowest possible X coordinate of a vertex.
+     *
+     * @return int
+     */
+    public function getLowestVertexX() : int {
+        $min = null;
+        foreach ($this->getStlFileContentArray()["facet-normals"] as $facetNormal) {
+            for ($i = 0; $i < 3; $i++) {
+                if (is_null($min)) {
+                    $min = $facetNormal["vertices"][$i][0];
+                    continue;
+                }
+                if ($min > $facetNormal["vertices"][$i][0]) {
+                    $min = $facetNormal["vertices"][$i][0];
+                }
+            }
+        }
+
+        return $min;
+    }
+
+    /**
+     * Highest possible X coordinate of a vertex.
+     *
+     * @return int
+     */
+    public function getHighestVertexX() : int {
+        $max = null;
+        foreach ($this->getStlFileContentArray()["facet-normals"] as $facetNormal) {
+            for ($i = 0; $i < 3; $i++) {
+                if (is_null($max)) {
+                    $max = $facetNormal["vertices"][$i][0];
+                    continue;
+                }
+                if ($max < $facetNormal["vertices"][$i][0]) {
+                    $max = $facetNormal["vertices"][$i][0];
+                }
+            }
+        }
+
+        return $max;
+    }
+
+    /**
+     * Remove all vertices with the lowest X values.
+     *
+     * @return STLMillingEdit
+     */
+    public function removeLowestXVertices() : STLMillingEdit {
+        $facetNormals = [];
+        $contentArray = $this->getStlFileContentArray();
+
+        foreach ($contentArray["facet-normals"] as $facetNormal) {
+            $skip = false;
+            for ($i = 0; $i < 3; $i++) {
+                if ($facetNormal["vertices"][$i][0] == $this->getLowestVertexX()) {
+                    $skip = true;
+                }
+            }
+            if (!$skip) {
+                $facetNormals[] = $facetNormal;
+            }
+        }
+
+        $contentArray["facet-normals"] = $facetNormals;
+
+        $this->setStlFileContentArray($contentArray);
+
+        return $this;
+    }
+
+    /**
+     * Remove all vertices with the highest X values.
+     *
+     * @return STLMillingEdit
+     */
+    public function removeHighestXVertices() : STLMillingEdit {
+        $facetNormals = [];
+        $contentArray = $this->getStlFileContentArray();
+
+        foreach ($contentArray["facet-normals"] as $facetNormal) {
+            $skip = false;
+            for ($i = 0; $i < 3; $i++) {
+                if ($facetNormal["vertices"][$i][0] == $this->getHighestVertexX()) {
+                    $skip = true;
+                }
+            }
+            if (!$skip) {
+                $facetNormals[] = $facetNormal;
+            }
+        }
+
+        $contentArray["facet-normals"] = $facetNormals;
+
+        $this->setStlFileContentArray($contentArray);
+
+        return $this;
+    }
+
+    /**
+     * Extract the actual milling objects.
+     *
+     * @return STLMillingEdit
+     */
+    public function extractMillingContent() : STLMillingEdit {
+        $this->removeHighestXVertices()->removeLowestXVertices();
+        return $this;
+    }
 }
--- cnc-api/tests/AppBundle/Service/STL/STLMillingEditTest.php
+++ cnc-api/tests/AppBundle/Service/STL/STLMillingEditTest.php
@@ -55,9 +55,9 @@ class STLMillingEditTest extends \PHPUnit_Framework_TestCase
     }
 
     public function testFileArrayIsSetByConstructor() {
-        $stlMillingEditor = \Mockery::mock(STLMillingEdit::class)->shouldAllowMockingProtectedMethods();
+        $stlMillingEditorMock = \Mockery::mock(STLMillingEdit::class)->shouldAllowMockingProtectedMethods();
 
-        $stlMillingEditor->shouldReceive('setStlFileContentArray')
+        $stlMillingEditorMock->shouldReceive('setStlFileContentArray')
             ->with($this->testStlArray)
             ->once();
 
@@ -65,8 +65,100 @@ class STLMillingEditTest extends \PHPUnit_Framework_TestCase
         $stlFileReaderReflectionConstructor = $stlFileReaderReflection->getConstructor();
 
         $stlFileReaderReflectionConstructor->invoke(
-            $stlMillingEditor,
+            $stlMillingEditorMock,
             $this->testStlArray
+        );
+    }
+
+    public function testLowestVertexX() {
+        $stlMillingEditor = new STLMillingEdit($this->testStlArray);
+
+        $this->assertEquals(
+            3,
+            $stlMillingEditor->getLowestVertexX()
+        );
+    }
+
+    public function testHighestVertexX() {
+        $stlMillingEditor = new STLMillingEdit($this->testStlArray);
+
+        $this->assertEquals(
+            19,
+            $stlMillingEditor->getHighestVertexX()
+        );
+    }
+
+    public function testRemoveLowestXVertices() {
+        $stlMillingEditor = new STLMillingEdit($this->testStlArray);
+
+        $this->assertEquals(
+            array(
+                "name" => "TEST2",
+                "facet-normals" => array(
+                    array(
+                        "coordinates" => array(
+                            10,
+                            11,
+                            12
+                        ),
+                        "vertices" => array(
+                            array(
+                                12, 14, 15
+                            ),
+                            array(
+                                16, 17, 18
+                            ),
+                            array(
+                                19, 20, 21
+                            )
+                        )
+                    )
+                )
+            ),
+            $stlMillingEditor->removeLowestXVertices()->getStlFileContentArray()
+        );
+    }
+
+    public function testRemoveHighestXVertices() {
+        $stlMillingEditor = new STLMillingEdit($this->testStlArray);
+
+        $this->assertEquals(
+            array(
+                "name" => "TEST2",
+                "facet-normals" => array(
+                    array(
+                        "coordinates" => array(
+                            0,
+                            1,
+                            2
+                        ),
+                        "vertices" => array(
+                            array(
+                                3, 4, 5
+                            ),
+                            array(
+                                5, 4, 6
+                            ),
+                            array(
+                                7, 8, 9
+                            )
+                        )
+                    )
+                )
+            ),
+            $stlMillingEditor->removeHighestXVertices()->getStlFileContentArray()
+        );
+    }
+
+    public function testExtractMillingContent() {
+        $stlMillingEditor = new STLMillingEdit($this->testStlArray);
+
+        $this->assertEquals(
+            array(
+                "name" => "TEST2",
+                "facet-normals" => array()
+            ),
+            $stlMillingEditor->extractMillingContent()->getStlFileContentArray()
         );
     }
 }
