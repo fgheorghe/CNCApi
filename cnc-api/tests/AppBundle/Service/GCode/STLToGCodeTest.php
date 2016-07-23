2ec50d6dcfad1bb3c203d12281eb37a9088bd19a
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2016-07-23 14:21:24 +0100

    Added stub test.

---
+++ cnc-api/tests/AppBundle/Service/GCode/STLToGCodeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AppBundle\Tests\Service\GCode;
+
+use Symfony\Component\DependencyInjection\Container;
+use AppBundle\Service\GCode\STLToGCode;
+
+class STLToGCodeTest extends \PHPUnit_Framework_TestCase
+{
+    // TODO.
+}
